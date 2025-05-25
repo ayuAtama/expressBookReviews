e588c36bcb674d643bf227a17b9170e149da4912
Wahyu Pratama <ayuramatama@gmail.com>
2025-05-25 16:23:08 +0700

    Finish: Task 7 Logging in as a registered user

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,8 +5,8 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  //write code to check is the username is valid
   let isUsernameValid = false;
   // users.forEach(user => {
   //   if (user.username === username) {
@@ -25,7 +25,7 @@ const isValid = (username)=>{ //returns boolean
   //   }
   // }
   for (let i = 0; i < users.length; i++) {
-    if (users[i].username === username){
+    if (users[i].username === username) {
       isUsernameValid = true;
       break; // Exit the loop once we find a match
     }
@@ -33,20 +33,44 @@ const isValid = (username)=>{ //returns boolean
   return isUsernameValid;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+  //write code to check if username and password match the one we have in records.
+  let isAuthenticated = false;
+  // users.forEach(user => {
+  //   if (user.username === username && user.password === password) {
+  //     isAuthenticated = true;
+  //   }
+  // })
+  // isAuthenticated = users.some(user => user.username === username && user.password === password);
+  // isAuthenticated = users.find(user => user.username === username && user.password === password) ? true : false;
+  // isAuthenticated = users.filter(user => user.username === username && user.password === password).length > 0;
+  // isAuthenticated = users.map(user => user.username === username && user.password === password).includes(true);
+  // isAuthenticated = users.reduce((acc, user) => acc || (user.username === username && user.password === password), false);
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username === username && users[i].password === password) {
+      isAuthenticated = true;
+      break; // Exit the loop once we find a match
+    }
+  }
+  return isAuthenticated;
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-// Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and Password are required" });
+  } else if (!isValid(username)) {
+    return res.status(401).json({ message: "User not found!" });
+  } else if (!authenticatedUser(username, password)) {
+    return res.status(401).json({ message: "Incorrect password!" });
+  } else { // create a token
+    let accessToken = jwt.sign({ data: username }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = { accessToken, username };
+    return res.status(200).json({ message: "User successfully logged in" }) && console.log("Here's the access token :" + accessToken);
+  }
 });
 
 module.exports.authenticated = regd_users;
